6b57de0fac65b0137d768d7ea84e74704bfaf989
etacalpha <etacalpha@gmail.com>
2020-03-30 11:49:41 -0400

    updated landing page

--- src/Components/Index/index.jsx
+++ src/Components/Index/index.jsx
@@ -5,7 +5,9 @@ import "./index.css";
 export default function Index() {
   return (
     <article className={"index"}>
-      
+      <h2 id={'firstName'}>Steven Beard</h2>
+      {/* <h2 id={'lastName'}>Beard</h2> */}
+      <p>an <span className={'standout'}> AWS Developer</span></p>
     </article>
   );
 }
